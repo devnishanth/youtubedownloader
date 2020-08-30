da3728005c1bca6e9bfabb0b1a7fa24586b572b2
devnishanth <67795990+devnishanth@users.noreply.github.com>
2020-08-30 18:31:49 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# from pytube import *
+from pytube import YouTube
+
+link = input("Enter a youtube link to download: ")
+yt= YouTube(link)
+
+#Showing details
+print("Title: ",yt.title)
+print("Number of views: ",yt.views)
+print("Length of video: ",yt.length)
+print("Rating of video: ",yt.rating)
+#Getting the highest resolution possible
+ys = yt.streams.get_highest_resolution()
+
+#Starting download
+print("Downloading...")
+ys.download()
+print("Download completed!!")
